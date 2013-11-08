c01b8e4cdecfc2c2ec2a424bc104c25409aab8b3
Maxin B. John <maxin.john@enea.com>
2013-11-08 14:06:59 +0100

    Various fixes and improvements by jbs
        -   Device context now held in cmd buffer
        -   Allow multiple motors at the same time
    
    Thanks to John Hanson for these improvements.

--- robotic_arm.c
+++ robotic_arm.c
@@ -12,6 +12,15 @@
  *
  * Derived from USB LED driver
  * Copyright (c) 2001 Greg Kroah-Hartman (greg@kroah.com)
+ *
+ * Modified by jbh (john.hanson@cccplusplus.co.uk)
+ *     -   Device context now held in cmd buffer
+ *     -   Allow multiple motors at the same time
+ *     -   Add parsed file which allows a single file to control the robot
+ *            characters 0..8 select the device to control
+ *            character S=stop, F=forward, R=reverse, E=emergency (stop all motors)
+ *            eg   echo "0F" >parsed    causes the gripper to close
+ *
  */
 
 #define DEBUG 1
@@ -22,6 +31,8 @@
 #include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/usb.h>
+#include <asm/uaccess.h>
+
 
 /* Version Information */
 #define DRIVER_VERSION "v0.1"
@@ -38,103 +49,20 @@ static struct usb_device_id id_table[] = {
 };
 
 MODULE_DEVICE_TABLE(usb, id_table);
-
+#define CMD_SIZE 3
 struct usb_robotic_arm {
 	struct usb_device *udev;
-	unsigned char led;
-	unsigned char gripmotor;
-	unsigned char motor2;
-	unsigned char motor3;
-	unsigned char motor4;
-	unsigned char basemotor;
+        u8            parse_point;
+        u8            cmd[CMD_SIZE];
 };
 
 static void update_robotic_arm(struct usb_robotic_arm *robotic_arm)
 {
 	int retval = 0;
-	u8 cmd[3];
-
-	cmd[0] = 0;
-	cmd[1] = 0;
-	cmd[2] = 0;
-
-	/*
-	 * Byte 2 controls LED light inside the grip.
-	 * If Bit 0 is 1, LED on, otherwise LED off
-	 *
-	 */
-
-	if (robotic_arm->led)
-		cmd[2] = 1;
-
-	/*
-	 * Byte 1 controls the Base Motor
-	 * If value is 1, Base Motor rotates clockwise
-	 * If value is 2, Base Motor rotates anti-clockwise
-	 * Otherwise, Base Motor is off
-	 *
-	 */
-
-	if (robotic_arm->basemotor == 1)
-		cmd[1] = 1;
-
-	else if (robotic_arm->basemotor == 2)
-		cmd[1] = 2;
-	else
-		cmd[1] = 0;
-
-	/*
-	 * Byte 0 controls the Grip
-	 * If value is 1, Grip tightens
-	 * If values is 2, Grip Looses
-	 *
-	 */
-
-	if (robotic_arm->gripmotor == 1)
-		cmd[0] = 1;
-	else if (robotic_arm->gripmotor == 2)
-		cmd[0] = 2;
-	/*
-	 * Byte 0 controls the Motor 2
-	 * If value is 1, Motor 2 (wrist) moves up
-	 * If value is 2, Motor 2 (wrist) moves down
-	 *
-	 */
-
-	else if (robotic_arm->motor2 == 1)
-		cmd[0] = 4;
-	else if (robotic_arm->motor2 == 2)
-		cmd[0] = 8;
-
-	/*
-	 * Byte 0 controls the Motor 3
-	 * If value is 1, Motor 3 (elbow) moves up
-	 * If value is 2, Motor 3 (elbow) moves down
-	 *
-	 */
-
-	else if (robotic_arm->motor3 == 1)
-		cmd[0] = 16;
-	else if (robotic_arm->motor3 == 2)
-		cmd[0] = 32;
-
-	/*
-	 * Byte 0 controls the Motor 4
-	 * If value is 1, Motor 4 (shoulder) moves up
-	 * If value is 2, Motor 4 (shoulder) moves down
-	 *
-	 */
-
-	else if (robotic_arm->motor4 == 1)
-		cmd[0] = 64;
-	else if (robotic_arm->motor4 == 2)
-		cmd[0] = 128;
-	else
-		cmd[0] = 0;
 
 	retval = usb_control_msg(robotic_arm->udev,
 				 usb_sndctrlpipe(robotic_arm->udev, 0),
-				 6, 0x40, 0x100, 0, cmd, 3, 0);
+				 6, 0x40, 0x100, 0, robotic_arm->cmd, 3, 0);
 
 
 	if (retval)
@@ -142,37 +70,171 @@ static void update_robotic_arm(struct usb_robotic_arm *robotic_arm)
 
 }
 
-#define show_set(value) \
+
+static ssize_t show_parsed(struct device *dev, struct device_attribute *attr,
+			   char *buf )
+
+{
+  struct usb_interface *intf = to_usb_interface(dev);
+  struct usb_robotic_arm *robotic_arm = usb_get_intfdata(intf);
+  u8 index;
+  ssize_t written;
+  written = 0;
+  for ( index=0;index<8;index++)
+  {
+    ssize_t count;
+    u8 shift, offset;
+    shift = ( 2 * ( index & 3 ) );
+    offset = ( index >> 2 );
+
+    count=sprintf(buf, "motor %d state %d\n", index, ( robotic_arm->cmd[offset]>>shift ) & 0x03) ;
+    buf += count;
+    written += count;
+  }
+  return written;
+}
+
+
+static ssize_t set_parsed(struct device *dev, struct device_attribute *attr,
+			  const char *buf, size_t count )
+{
+  size_t written;
+  struct usb_interface *intf = to_usb_interface(dev);
+  struct usb_robotic_arm *robotic_arm = usb_get_intfdata(intf);
+  u8 update_needed = 0;
+  written = 0;
+  while ( count-- > 0 )
+  {
+    char file_data;
+    file_data = *buf;
+    if (( file_data >= '0' ) && ( file_data <= '9' ) )
+    {
+      robotic_arm->parse_point = ( file_data - '0' );
+    }
+    else if ( file_data == 'E' )   /* Emergency stop everything */
+    {
+      u8 offset;
+      for (offset=0;offset<CMD_SIZE;offset++)
+      {
+        robotic_arm->cmd[offset] = 0;
+      }
+      update_needed=1;
+    }
+    else
+    {
+      u8 shift, offset;
+      shift = ( 2 * ( robotic_arm->parse_point & 3 ) );
+      offset = ( robotic_arm->parse_point >> 2 );
+      if ( offset < CMD_SIZE )
+      {
+        switch ( file_data )
+        {
+	  case 'F' :   /* Forward */
+            robotic_arm->cmd[offset] &= ~( 0x03 << shift );
+            robotic_arm->cmd[offset] |= ( 0x01  << shift );
+            update_needed = 1;
+	    break;
+	  case 'R' :  /* Reverse */
+            robotic_arm->cmd[offset] &= ~( 0x03 << shift );
+            robotic_arm->cmd[offset] |= ( 0x02  << shift );
+            update_needed = 1;
+	    break;
+	  case 'S' :   /* Stop */
+            robotic_arm->cmd[offset] &= ~( 0x03 << shift );
+            robotic_arm->cmd[offset] |= ( 0x00  << shift );
+            update_needed = 1;
+	    break;
+	}
+      }
+    }
+    buf++;
+    written++;
+ }
+ if ( update_needed )
+ {
+   update_robotic_arm(robotic_arm);
+ }
+ return written;
+
+}
+
+
+static DEVICE_ATTR(parsed, S_IRUGO | S_IWUSR, show_parsed, set_parsed);
+
+
+
+static ssize_t show_common(struct device *dev, struct device_attribute *attr,
+			   char *buf, u8 index)
+{
+  struct usb_interface *intf = to_usb_interface(dev);
+  struct usb_robotic_arm *robotic_arm = usb_get_intfdata(intf);
+  u8 shift, offset;
+  shift = ( 2 * ( index & 3 ) );
+  offset = ( index >> 2 );
+
+  return sprintf(buf, "%d\n",
+    ( robotic_arm->cmd[offset]>>shift ) & 0x03) ;
+
+}
+
+
+
+
+static ssize_t set_common(struct device *dev, struct device_attribute *attr,
+			  const char *buf, size_t count, u8 index)
+{
+  size_t written;
+  struct usb_interface *intf = to_usb_interface(dev);
+  struct usb_robotic_arm *robotic_arm = usb_get_intfdata(intf);
+  u8 shift, offset;
+  shift = ( 2 * ( index & 3 ) );
+  offset = ( index >> 2 );
+  written = 0;
+  while ( count-- > 0 )
+  {
+    char file_data;
+    file_data = *buf;
+    switch ( file_data )
+    {
+      case '0' :
+      case '1' :
+      case '2' : 
+        robotic_arm->cmd[offset] &= ~( 0x03 << shift );
+        robotic_arm->cmd[offset] |= ( ( file_data & 0x03 )  << shift );
+	break;
+    }
+    buf++;
+    written++;
+ }
+ update_robotic_arm(robotic_arm);
+ return written;
+
+}
+
+
+
+
+
+#define show_set(value, index)						\
 static ssize_t show_##value(struct device *dev, struct device_attribute *attr,\
 			char *buf)					\
 {									\
-	struct usb_interface *intf = to_usb_interface(dev);		\
-	struct usb_robotic_arm *robotic_arm = usb_get_intfdata(intf);	\
-									\
-	return sprintf(buf, "%d\n", robotic_arm->value);		\
+  return show_common( dev, attr, buf, index );                          \
 }                                                                       \
 static ssize_t set_##value(struct device *dev, struct device_attribute *attr,\
 				const char *buf, size_t count)		\
 {                                                                       \
-	struct usb_interface *intf = to_usb_interface(dev);             \
-	struct usb_robotic_arm *robotic_arm = usb_get_intfdata(intf);   \
-	int ret = -ENXIO;						\
-	unsigned long val;						\
-									\
-	ret = kstrtoul(buf, 0, &val);					\
-	if (ret)							\
-		return ret;						\
-	robotic_arm->value = val;					\
-	update_robotic_arm(robotic_arm);                                \
-	return count;                                                   \
+  return set_common( dev, attr, buf, count, index );                    \
 }                                                                       \
 static DEVICE_ATTR(value, S_IRUGO | S_IWUSR, show_##value, set_##value);
-show_set(led);
-show_set(gripmotor);
-show_set(motor2);
-show_set(motor3);
-show_set(motor4);
-show_set(basemotor);
+
+
+show_set(led, 8);
+show_set(gripmotor,0);
+show_set(motor2, 1);
+show_set(motor3, 2);
+show_set(motor4, 3);
+show_set(basemotor, 4);
 
 static int robotic_arm_probe(struct usb_interface *interface,
 			     const struct usb_device_id *id)
@@ -210,6 +272,9 @@ static int robotic_arm_probe(struct usb_interface *interface,
 	retval = device_create_file(&interface->dev, &dev_attr_basemotor);
 	if (retval)
 		goto error;
+	retval = device_create_file(&interface->dev, &dev_attr_parsed);
+	if (retval)
+		goto error;
 
 	dev_info(&interface->dev, "USB ROBOTIC ARM now attached\n");
 	return 0;
@@ -221,6 +286,7 @@ error:
 	device_remove_file(&interface->dev, &dev_attr_motor3);
 	device_remove_file(&interface->dev, &dev_attr_motor4);
 	device_remove_file(&interface->dev, &dev_attr_basemotor);
+	device_remove_file(&interface->dev, &dev_attr_parsed);
 
 	usb_set_intfdata(interface, NULL);
 	usb_put_dev(dev->udev);
@@ -242,6 +308,7 @@ static void robotic_arm_disconnect(struct usb_interface *interface)
 	device_remove_file(&interface->dev, &dev_attr_motor3);
 	device_remove_file(&interface->dev, &dev_attr_motor4);
 	device_remove_file(&interface->dev, &dev_attr_basemotor);
+	device_remove_file(&interface->dev, &dev_attr_parsed);
 
 	/* first remove the files, then set the pointer to NULL */
 	usb_set_intfdata(interface, NULL);
